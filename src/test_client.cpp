afc4f9427c4ea74067c5d3435bca27f3fc4fd1f6
Patrick Mania <pmania@tzi.de>
2014-07-04 22:42:03 +0200

    added a wrapper for the actual test client, so nobody has to construct a jsonrpc::HttpClient everytime

---
+++ src/test_client.cpp
@@ -0,0 +1,17 @@
+#include "gztest/test_client.h"
+
+using namespace gztest;
+
+TestClient::TestClient(std::string address)
+{
+    this->client = new GazeboTestClient(new jsonrpc::HttpClient(address));
+}
+TestClient::~TestClient()
+{
+    delete this->client;
+}
+
+bool TestClient::onObject(const std::string& object, const std::string& surface){
+  return this->client->onObject(object,surface);
+}
+

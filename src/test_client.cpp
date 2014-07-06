b666f14c91413753429e3c026ccb99fc0b2b918d
Patrick Mania <pmania@tzi.de>
2014-07-06 22:35:56 +0200

    added comments and modified declarations to comply with the style guide

--- src/test_client.cpp
+++ src/test_client.cpp
@@ -2,16 +2,20 @@
 
 using namespace gztest;
 
+//////////////////////////////////////////////////////////
 TestClient::TestClient(std::string address)
 {
     this->client = new GazeboTestClient(new jsonrpc::HttpClient(address));
 }
+
+//////////////////////////////////////////////////////////
 TestClient::~TestClient()
 {
     delete this->client;
 }
 
-bool TestClient::onObject(const std::string& object, const std::string& surface){
+//////////////////////////////////////////////////////////
+bool TestClient::OnObject(const std::string& object, const std::string& surface){
   return this->client->onObject(object,surface);
 }
 
